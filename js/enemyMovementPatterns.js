66b07b1a2521c04b57da50a788969970ccbf97b4
Tree Casiano <tree.pdx@gmail.com>
2023-08-28 07:29:43 -0700

    Finish making bats flap and move at random

--- js/enemyMovementPatterns.js
+++ js/enemyMovementPatterns.js
@@ -9,21 +9,44 @@ CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
+let gameFrame = 0;
+
 class Enemy {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height = 100;
-    this.speed = Math.random() * 4 - 2;
+    this.image = new Image();
+    this.image.src = "images/enemy1.png";
+    this.spriteWidth = 293;
+    this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
   }
-  draw() {
-    ctx.strokeStyle = "magenta";
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
-  }
+
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
+    this.x += Math.random() * 5 - 2.5;
+    this.y += Math.random() * 5 - 2.5;
+
+    // only run the code only every two loops of main animation loop (this slows down the movement)
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
+  }
+
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
@@ -37,6 +60,7 @@ function animate() {
     enemy.update();
     enemy.draw();
   });
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 
