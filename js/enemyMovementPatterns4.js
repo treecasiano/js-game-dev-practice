0cda8491f5714bc71354ec7084c5389ea0c7fd59
Tree Casiano <tree.pdx@gmail.com>
2023-09-07 15:12:22 -0700

    Add 4th enemy movement pattern

---
+++ js/enemyMovementPatterns4.js
@@ -0,0 +1,81 @@
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById("canvas6");
+const ctx = canvas.getContext("2d");
+
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
+
+const numberOfEnemies = 100;
+const enemiesArray = [];
+
+let gameFrame = 0;
+
+class Enemy {
+  constructor() {
+    this.image = new Image();
+    this.image.src = "images/enemy4.png";
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 213;
+    this.spriteHeight = 213;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.newX = Math.random() * (canvas.height - this.height);
+    this.newY = Math.random() * (canvas.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.interval = Math.floor(Math.random() * 200 + 50);
+  }
+
+  update() {
+    if (gameFrame % this.interval === 0) {
+      this.newX = Math.random() * (canvas.height - this.height);
+      this.newY = Math.random() * (canvas.height - this.height);
+    }
+
+    let dx = this.x - this.newX;
+    let dy = this.y - this.newY;
+
+    this.x -= dx / 20;
+    this.y -= dy / 20;
+
+    if (this.x + this.width < 0) this.x = canvas.width;
+
+    // only run the code only every two loops of main animation loop (this slows down the movement)
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
+  }
+
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+for (let i = 0; i < numberOfEnemies; i++) {
+  enemiesArray.push(new Enemy());
+}
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemiesArray.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+
+animate();
